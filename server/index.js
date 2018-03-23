8bf59481c20b25b37531c04b504ddb5db9b5277c
Marina <marinablack1992@gmail.com>
2018-03-23 17:12:44 -0600

    http not https

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json()); // body-parser is a middleware that gets the entire 
 app.use(cors()); // idk what this shit does i just have it cuz sometimes stuff breaks if you don't. i think its something to do with making your http requests more secure.
 
 // 'endpoints' are urls that you hit in the back end. they will build off of your port/domain.
-// if you wanted to hit your endpoint in postman the full URL would be: https://localhost:3001/api/create_address
+// if you wanted to hit your endpoint in postman the full URL would be: http://localhost:3001/api/create_address
 
 // '/api/get_address/:addressID' -> the : indicates a parameter AKA a required value. request will fail if you don't fillout the parameter.
 // when you make a request to that url with param, it would look something like this:
-// https://localhost:3001/api/get_address/address_id_293394567542
+// http://localhost:3001/api/get_address/address_id_293394567542
 
 app.post('/api/create_address', addrCtrl.createAddress); // 'post' indicates that we are creating something.
 app.get('/api/get_address/:addressID', addrCtrl.getAddress);  // 'get' indicates that we are retreiving something.
